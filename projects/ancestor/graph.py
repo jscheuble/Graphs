860a37b7dc36f13dec080a0632512b1ef4225235
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-08-11 15:28:09 -0700

    stack and graph

---
+++ projects/ancestor/graph.py
@@ -0,0 +1,16 @@
+class Graph:
+
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('vertex does not exist')
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
