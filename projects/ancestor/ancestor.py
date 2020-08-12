a1e42a29cf995fe31c234ed84825dddea2c3d8e0
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-08-11 21:50:42 -0700

    mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,48 @@
+from stack import Stack
+from collections import defaultdict
+
+
+def dfs(starting_vertex, g):
+    if starting_vertex not in g:
+        return []
+
+    stack = Stack()
+    stack.push([starting_vertex])
+
+    visited = []
+
+    while stack.size():
+        path = stack.pop()
+
+        # Get the last vertex in the path
+        vertex = path[-1]
+
+        # mark visited
+        if vertex not in visited:
+            visited.append(vertex)
+
+        # add path to parents to the stack
+        # if the vertex has no parents, it sets dict value to empty list
+        for parent in g.get(vertex, []):
+            stack.push(path + [parent])
+
+    # return the last visited vertex, this will be the highest level parent
+    return visited[-1]
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = {}
+
+    # loop over ancestors, add
+    for parent, child in ancestors:
+        if child not in g:
+            g[child] = [parent]
+        else:
+            g[child].append(parent)
+
+    # return -1 if starting node has no parents
+    if starting_node not in g:
+        return -1
+
+    # else return depth first search
+    return dfs(starting_node, g)
